9be167b35542cf861928ea0f9b4afd78e6811e4f
anmu123 <1606423847@qq.com>
2019-04-14 20:45:25 +0800

    数据渲染以及删除任务完成

--- js/app.js
+++ js/app.js
@@ -3,4 +3,31 @@
 
 	// Your starting point. Enjoy the ride!
 
+	const vm = new Vue({
+		el: '.todoapp',
+		// 1.准备数据,渲染页面
+		data : {
+			list:[
+				{ id : 1 , name : '春天' ,done : true},
+				{ id : 2 , name : '长裙' ,done : false},
+				{ id : 3 , name : '樱花' ,done : true}
+			]
+		},
+		methods :{
+			// 2.删除任务
+			destodo(id){
+				console.log(id);
+				// 方法一:过滤
+				this.list = this.list.filter(item => item.id != id)
+				// console.log(this.list);	
+			}
+			// 3.添加任务
+			// 4.修改任务
+			// 5.隐藏底部
+		}
+
+
+	})
+
+
 })(window);
